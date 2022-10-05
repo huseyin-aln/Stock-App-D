3dc4aa2064229d70843f936ef6d53c7edd52daad
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-10-05 14:32:47 +0300

    RegisterView

--- users/serializers.py
+++ users/serializers.py
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fileds = (
+        fields = (
             'username',
             'email',
             'first_name',
@@ -31,3 +31,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password',
             'password1'
         )
+
+    def validate(self, data):
+        if data['password'] != data['password1']:
+            raise serializers.ValidationError(
+                {"password" : "Password didn't match..."}
+            )
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        validated_data.pop('password1')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
